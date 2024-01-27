084e7a7bc75a577644d522f35deabee24f59a893
AnasDol <anastasiadol2002@gmail.com>
2024-01-27 14:36:00 +0700

    reads from kafka

--- src/main/java/StructuredStreamingApp.java
+++ src/main/java/StructuredStreamingApp.java
@@ -19,20 +19,35 @@ public class StructuredStreamingApp {
                 .getOrCreate();
 
         // Create DataFrame representing the stream of input lines from connection to localhost:9999
-        Dataset<Row> lines = spark
+//        Dataset<Row> lines = spark
+//                .readStream()
+//                .format("socket")
+//                .option("host", "localhost")
+//                .option("port", 9999)
+//                .load();
+
+        Dataset<Row> df = spark
                 .readStream()
-                .format("socket")
-                .option("host", "localhost")
-                .option("port", 9999)
+                .format("kafka")
+                .option("kafka.bootstrap.servers", "kafka:9092")
+                .option("subscribe", "test2")
                 .load();
 
         // Split the lines into words
-        Dataset<String> words = lines
-                .as(Encoders.STRING())
-                .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
+//        Dataset<String> words = lines
+//                .as(Encoders.STRING())
+//                .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
+         StreamingQuery query = df
+                 .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+                 .writeStream()
+                 .outputMode("append")
+                 .format("console")
+                 .start();
 
         // Generate running word count
-        Dataset<Row> wordCounts = words.groupBy("value").count();
+//        Dataset<Row> wordCounts = words.groupBy("value").count();
+
+
 
 //        Dataset<Row> result = inputData.selectExpr("value as input")
 //                .groupBy("input")
@@ -40,10 +55,10 @@ public class StructuredStreamingApp {
 
 
         // Start running the query that prints the running counts to the console
-        StreamingQuery query = wordCounts.writeStream()
-                .outputMode("complete")
-                .format("console")
-                .start();
+//        StreamingQuery query = wordCounts.writeStream()
+//                .outputMode("complete")
+//                .format("console")
+//                .start();
 
         query.awaitTermination();
 
