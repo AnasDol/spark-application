fc4fba97b7f0b9f0ab52b2ba4349fa00893dc77d
AnasDol <anastasiadol2002@gmail.com>
2024-02-06 18:47:36 +0700

    + postgresql

--- src/main/java/StructuredStreamingApp.java
+++ src/main/java/StructuredStreamingApp.java
@@ -4,6 +4,8 @@ import org.apache.spark.sql.streaming.StreamingQuery;
 
 import org.apache.spark.sql.streaming.StreamingQueryException;
 import java.util.concurrent.TimeoutException;
+import static org.apache.spark.sql.avro.functions.*;
+import static org.apache.spark.sql.functions.*;
 
 import java.util.Arrays;
 
@@ -11,56 +13,62 @@ public class StructuredStreamingApp {
 
     public static void main(String[] args) throws StreamingQueryException, TimeoutException {
 
-        // Initialize SparkSession
         SparkSession spark = SparkSession
                 .builder()
                 .appName("JavaStructuredNetworkWordCount")
-                //.master("local[2]") // Use local mode with 2 cores, adjust as needed
                 .getOrCreate();
 
-        // Create DataFrame representing the stream of input lines from connection to localhost:9999
-//        Dataset<Row> lines = spark
-//                .readStream()
-//                .format("socket")
-//                .option("host", "localhost")
-//                .option("port", 9999)
-//                .load();
-
-        Dataset<Row> df = spark
+        Dataset<Row> kafkaDF = spark
                 .readStream()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "kafka:9092")
                 .option("subscribe", "test2")
                 .load();
 
-        // Split the lines into words
-//        Dataset<String> words = lines
-//                .as(Encoders.STRING())
-//                .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
-         StreamingQuery query = df
-                 .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-                 .writeStream()
-                 .outputMode("append")
-                 .format("console")
-                 .start();
+        Dataset<Row> df = kafkaDF
+                .selectExpr("CAST(value AS STRING)")
+                .select(split(new Column("value"), ",").alias("csv_values"))
+                .selectExpr("csv_values[0] AS ip", "CAST(csv_values[1] AS INT) AS num");
 
-        // Generate running word count
-//        Dataset<Row> wordCounts = words.groupBy("value").count();
+        Dataset<Row> jdbcDF = spark
+                .read()
+                .format("jdbc")
+                .option("url", "jdbc:postgresql://host.docker.internal:5432/diploma")
+                .option("dbtable", "public.table1")
+                .option("user", "postgres")
+                .option("password", "7844")
+                .load();
 
+        // Соединяем таблицы по ip и заполняем недостающие numы
+        Dataset<Row> enrichedDF_num = df.join(jdbcDF, df.col("ip").equalTo(jdbcDF.col("ip")), "left_outer")
+                .select(
+                        df.col("ip"),
+                        df.col("num"),
+                        jdbcDF.col("ip").alias("jdbcIp"),
+                        jdbcDF.col("num").alias("jdbcNum"),
+                        when(df.col("num").isNull(), jdbcDF.col("num").alias("smartNum"))
+                                .otherwise(df.col("num")).alias("smartNum")
+                );
 
+        // Соединяем по num и заполняем ip
+        Dataset<Row> enrichedDF_ip = enrichedDF_num.join(jdbcDF, enrichedDF_num.col("num").equalTo(jdbcDF.col("num")), "left_outer")
+                .select(
+                        when(enrichedDF_num.col("ip").isNull(), jdbcDF.col("ip"))
+                                .otherwise(enrichedDF_num.col("ip")).alias("ip"),
+                        enrichedDF_num.col("num")
+                );
 
-//        Dataset<Row> result = inputData.selectExpr("value as input")
-//                .groupBy("input")
-//                .count();
+        StreamingQuery query = enrichedDF_num
+                .writeStream()
+                .outputMode("append")
+                .format("console")
+                .start();
 
-
-        // Start running the query that prints the running counts to the console
-//        StreamingQuery query = wordCounts.writeStream()
-//                .outputMode("complete")
-//                .format("console")
-//                .start();
-
-        query.awaitTermination();
+        try {
+            query.awaitTermination();
+        } catch (StreamingQueryException e) {
+            e.printStackTrace();
+        }
 
     }
 }
